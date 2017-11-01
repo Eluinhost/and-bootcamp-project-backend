d52e78929a68dd14eface4c3d6247037f9224378
Armin <arminpudic@yahoo.co.uk>
2017-11-01 15:11:48 +0000

    Adds implementation for error handling an unavailable database

--- src/endpoints/getUsers.js
+++ src/endpoints/getUsers.js
@@ -1,6 +1,13 @@
 const getUsers = async (ctx) => {
-  const { rows } = await ctx.db.getUsers();
-  ctx.body = rows;
+  try {
+    const { rows } = await ctx.db.getUsers();
+    ctx.body = rows;
+    ctx.response.status = 200;
+  }
+  catch (err) {
+    ctx.body = err.message;
+    ctx.response.status = 503;
+  }
 };
 
 module.exports = getUsers;
--- src/endpoints/getUsers.spec.js
+++ src/endpoints/getUsers.spec.js
@@ -1,5 +1,21 @@
  const getUsers = require('./getUsers');
 
+test('Returns empty list with no data', async () => {
+  const rows = [];
+
+  const ctx = {
+    db: {
+      getUsers: async () => ({ rows }),
+    },
+    response: {}
+  };
+
+  await getUsers(ctx);
+
+  expect(ctx.body.length).toEqual(0);
+  expect(ctx.response.status).toEqual(200);
+});
+
 test('getUsers should return an array of users', async () => {
   const rows = ['item1', 'item2'];
 
@@ -7,9 +23,26 @@ test('getUsers should return an array of users', async () => {
     db: {
       getUsers: async () => ({ rows }),
     },
+    response: {}
   };
 
   await getUsers(ctx);
 
   expect(ctx.body.length).toEqual(2);
+  expect(ctx.body).
+  expect(ctx.response.status).toEqual(200);
+});
+
+test('Returns error if database unavailable', async () => {
+  const ctx = {
+    db: {
+      getUsers: async () => Promise.reject(new Error("Error connecting to database")),
+    },
+    response: {}
+  };
+
+  await getUsers(ctx);
+
+  expect(ctx.body).toEqual("Error connecting to database");
+  expect(ctx.response.status).toEqual(503);
 });
